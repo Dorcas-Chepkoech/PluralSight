315fd806900cbae5ff2cd7e4b3b20b8553ec03a5
just-atem <dorrchep@gmail.com>
2019-10-31 09:57:38 +0300

    Just JavaScript Practice

---
+++ Desktop/PluralSight/BlackJack.js
@@ -0,0 +1,15 @@
+//blackjack
+//by mz
+let deck = [
+    'Ace of Spades',
+    'Two of Spades',
+    'Three of Spades'
+];
+let playerCards = [deck[0], deck[1]];
+
+
+console.log('Welcome to BlackJack');
+console.log('You are dealt:');
+console.log('' + playerCards[0]);
+console.log('' + playerCards[1]);
+
---
+++ Desktop/PluralSight/Hello-World.js
@@ -0,0 +1,18 @@
+console.log('Hello World');
+
+let productName = 'hammer';
+console.log(productName);
+
+let first = 9;
+let second = 7;
+
+console.log(first+second);
+console.log(first-second);
+console.log(first*second);
+console.log(first/second);
+console.log(first%second);
+
+let value = 4.1 +4.3;
+console.log(value);
+
+let cities = ['New York', 'Tokyo'];
